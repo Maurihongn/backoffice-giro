f7d40498133cb265b54b1342ccf4218fea16a673
Mauricio Hongn <mauriciohongn282@gmail.com>
2025-05-19 09:36:27 -0300

    suspense use sear params

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import SignInForm from "./_components/SignInForm";
 
 export default function Page() {
   return (
     <div className="relative overflow-y-auto w-full flex items-center justify-center p-4">
       <div className="z-10">
-        <SignInForm />
+        <Suspense>
+          <SignInForm />
+        </Suspense>
       </div>
     </div>
   );
